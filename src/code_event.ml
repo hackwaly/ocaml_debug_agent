b0e2c8370fe6f26c01193d45e46019f3115dcc20
文宇祥 <hackwaly@qq.com>
2020-12-22 11:51:28 +0800

    WIP

--- src/code_event.ml
+++ src/code_event.ml
@@ -5,3 +5,9 @@ type t = { frag : int; event : Instruct.debug_event }
 let stacksize t = t.event.ev_stacksize
 
 let to_pc t = { frag = t.frag; pos = t.event.ev_pos }
+
+let lexing_location t =
+  t.event.ev_loc
+
+let lexing_position t =
+  Debug_event.lexing_position t.event
